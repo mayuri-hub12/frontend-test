27172d8d481fa6867a523533137b20823f0b56bd
mayuri012 <mayuridoifode2002@gmail.com>
2024-10-05 13:47:12 +0530

    new files

---
+++ style.js
@@ -0,0 +1,33 @@
+
+//converting celsius into fahrenheit
+
+let celsius=58;
+let fahrenheit=(celsius*9/5)+32;
+
+console.log("converted value of celsius is " ,fahrenheit);
+
+
+//multiplication table
+
+// let num=2;
+function MutiplicationTable(num){
+    for(  let i=1; i<=10; i++);
+    {
+        console.log(num*2);
+    }
+    
+}
+MutiplicationTable(2);
+
+
+
+//positive negative number
+
+let a1=2;
+
+if(a1>=0){
+    console.log("number is positive");
+}
+else{
+    console.log("number is positive");
+}
